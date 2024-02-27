3a4bdf343ddf674db81b7ccb41930ca6bf82de41
Leonidus1995 <gurjitsinghu0@gmail.com>
2024-02-26 21:21:22 -0500

    biostring.R file modified

--- L9-R-biostring.R
+++ L9-R-biostring.R
@@ -2,7 +2,7 @@
 library(Biostrings)
 
 # read sequence
-dna <- readDNAStringSet("/home/cbergman/GENE8940/L9-R-test.fasta")
+dna <- readDNAStringSet("/home/fg69001/GENE8940/L9-R-test.fasta")
 
 # sequence transformation
 dna_complement <- complement(dna)
@@ -10,4 +10,4 @@ dna_reversed = reverse(dna)
 dna_reverse_complement = reverseComplement(dna)
 
 # write new sequence as fasta file
-writeXStringSet(dna_reverse_complement, "/work/gene8940/cbergman/test_reverse_complement.fasta")
+writeXStringSet(dna_reverse_complement, "/work/gene8940/fg69001/test_reverse_complement.fasta")
