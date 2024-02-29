d35410d68852766e3128c48953102081c4e0f86d
Leonidus1995 <gurjitsinghu0@gmail.com>
2024-02-29 11:40:19 -0500

    Ok so finally the last draft for HW2. I am positive

--- Homework_2.sh
+++ Homework_2.sh
@@ -25,35 +25,35 @@ module load SAMtools/1.14-GCC-11.2.0
 module load ucsc/443
 
 # Convert the GFF file to BED format
-convert2bed --input=gff < ecoli_MG1655_ver58.gff > ecoli58.bed
+convert2bed --input=gff < ecoli_MG1655_ver58.gff > $OUTDIR/ecoli58.bed
 
 # Filter the BED file to create new BED file with only CDS regions
-grep "ID=CDS" ecoli58.bed > ecoli58_1_cds.bed
+grep "ID=CDS" ecoli58.bed > $OUTDIR/ecoli58_1_cds.bed
 
 # Create a genome index file for the genome sequence fasta file
 samtools faidx ecoli_MG1655_ver58.fa
 
 # Extract the first two columns of the index file and save it in the text file
-cut -f1,2 ecoli_MG1655_ver58.fa.fai > ecoli_MG1655_index.txt
+cut -f1,2 ecoli_MG1655_ver58.fa.fai > $OUTDIR/ecoli_MG1655_index.txt
 
 # Find the non CDS regions
-bedtools complement -i ecoli58_1_cds.bed -g ecoli_MG1655_index.txt > ecoli_intergenic.bed
+bedtools complement -i ecoli58_1_cds.bed -g ecoli_MG1655_index.txt > $OUTDIR/ecoli_intergenic.bed
 
 # Extract FASTA sequences for CDS and non-CDS regions from a reference genome based on respective annotated BED files
-bedtools getfasta -fi ecoli_MG1655_ver58.fa -bed ecoli58_1_cds.bed -fo ecoli58_cds.fa
-bedtools getfasta -fi ecoli_MG1655_ver58.fa -bed ecoli_intergenic.bed -fo ecoli58_intergenic.fa
+bedtools getfasta -fi ecoli_MG1655_ver58.fa -bed ecoli58_1_cds.bed -fo $OUTDIR/ecoli58_cds.fa
+bedtools getfasta -fi ecoli_MG1655_ver58.fa -bed ecoli_intergenic.bed -fo $OUTDIR/ecoli58_intergenic.fa
 
 # Calculating the %GC content of CDS and non-CDS regions
-faCount ecoli58_cds.fa > results_cds.txt
+faCount ecoli58_cds.fa > $OUTDIR/results_cds.txt
 awk 'NR > 1 { gc_count += $4 + $5; total_count += $6 + $3 + $4 + $5} END { 
     gc_content = (gc_count / total_count) * 100; 
     print("Overall GC content:", gc_content)}' results_cds.txt > $OUTDIR/summary_cds.txt
 
-faCount ecoli58_intergenic.fa > results_intergenic.txt
+faCount ecoli58_intergenic.fa > $OUTDIR/results_intergenic.txt
 awk 'NR > 1 { gc_count += $4 + $5; total_count += $6 + $3 + $4 + $5} END { 
     gc_content = (gc_count / total_count) * 100; 
     print("Overall GC content:", gc_content)}' results_intergenic.txt > $OUTDIR/summary_intergenic.txt
 
 # faCount -summary command also give the similar results for GC content
-faCount -summary ecoli58_cds.fa > facount_cds.txt
-faCount -summary ecoli58_intergenic.fa > facount_intergenic.txt
+faCount -summary ecoli58_cds.fa > $OUTDIR/facount_cds.txt
+faCount -summary ecoli58_intergenic.fa > $OUTDIR/facount_intergenic.txt
