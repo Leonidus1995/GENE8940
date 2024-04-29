73b84112dae5b1fb8d992ebb1d8cca3a7566932f
Leonidus1995 <gurjitsinghu0@gmail.com>
2024-04-29 19:53:46 -0400

    DESeq2_final_project.sh file edited

--- DESeq2_final_project.sh
+++ DESeq2_final_project.sh
@@ -10,6 +10,6 @@
 #SBATCH --mail-user=fg69001@uga.edu                                  
 #SBATCH --mail-type=END,FAIL 
 
-module load R/4.1.0-foss-2019b
+module load R/4.3.2-foss-2022b
 
 R --no-save < /home/fg69001/GENE8940/DESeq2_final_project.R
