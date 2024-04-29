0664f9fb316b84faf54bad0e3a202fa24f8bfc80
Leonidus1995 <gurjitsinghu0@gmail.com>
2024-04-29 19:50:09 -0400

    DESeq2_final_project.sh file added

---
+++ DESeq2_final_project.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=DESeq2_final_project		                            
+#SBATCH --partition=batch		                                    
+#SBATCH --ntasks=1			                                        
+#SBATCH --cpus-per-task=24		                                     
+#SBATCH --mem=50gb			                                        
+#SBATCH --time=72:00:00  		                                    
+#SBATCH --output=/work/gene8940/fg69001/Final_project/log.%j.out		 
+#SBATCH --error=/work/gene8940/fg69001/Final_project/log.%j.err        
+#SBATCH --mail-user=fg69001@uga.edu                                  
+#SBATCH --mail-type=END,FAIL 
+
+module load R/4.1.0-foss-2019b
+
+R --no-save < /home/fg69001/GENE8940/DESeq2_final_project.R
