990ce0f7a370b7e7e32a8d3cc7639cc030017f57
Leonidus1995 <gurjitsinghu0@gmail.com>
2024-04-28 20:52:09 -0400

    Bowtie.sh file updated

--- Bowtie.sh
+++ Bowtie.sh
@@ -17,10 +17,10 @@ module load SAMtools/1.14-GCC-11.2.0
 
 bowtie2-build /work/gene8940/fg69001/Final_project/bowtie/cotton_rna.fa /work/gene8940/fg69001/Final_project/bowtie/ref_index/cotton 
 
-reference_index="/work/gene8940/fg69001/final_project_test/reference_index"
+reference_index="/work/gene8940/fg69001/Final_project/bowtie/ref_index"
 for i in CCI1 CCI21 CCI3 CNI1 CNI2 CNI11
 do
-    bowtie2 -x $reference_index/cotton_index.idx -1 /work/gene8940/fg69001/Final_project/cotton_sequences/${i}_1.fq.gz -2 /work/gene8940/fg69001/Final_project/cotton_sequences/${i}_2.fq.gz -S /work/gene8940/fg69001/Final_project/bowtie/mapped/${i}.sam --threads 10
+    bowtie2 -x $reference_index/cotton -1 /work/gene8940/fg69001/Final_project/cotton_sequences/${i}_1.fq.gz -2 /work/gene8940/fg69001/Final_project/cotton_sequences/${i}_2.fq.gz -S /work/gene8940/fg69001/Final_project/bowtie/mapped/${i}.sam --threads 10
 done
 
 
